3b49fd35766d43f5716c8613b2aa6fc06a2bcefe
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2022-05-25 13:16:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='Computer Controller',
-    version='0.00.01',
-    packages=[''],
-    url='https://github.com/UniqueName12345/computer-controller',
-    license='Apache-2.0 License',
-    author='smurf',
-    author_email='',
-    description='A computer controller aimed for people migrating OSes or distros.'
+    name='Computer Controller',  # Name of package.
+    version='0.00.01',  # Version of package.
+    packages=[''],  # TODO: Add packages (IDK what to do here)
+    url='https://github.com/UniqueName12345/computer-controller',  # URL to package.
+    license='Apache-2.0 License',  # License.
+    author='UniqueName12345',  # Author.
+    author_email='',  # Author email (blank).
+    description='A computer controller aimed for people migrating OSes or distros.'  # Description.
 )
