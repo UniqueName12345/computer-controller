b18f8a4c60a48a514ee053766ca6a44afdb9554a
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2022-05-25 13:14:48 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Computer Controller',
+    version='0.00.01',
+    packages=[''],
+    url='https://github.com/UniqueName12345/computer-controller',
+    license='Apache-2.0 License',
+    author='smurf',
+    author_email='',
+    description='A computer controller aimed for people migrating OSes or distros.'
+)
